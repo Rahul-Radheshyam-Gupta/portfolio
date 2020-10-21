c5480d79e2bd856bb0e9259a9075c4b88a53ee35
Rahul-Radheshyam-Gupta <mrrahulgupta0096@gmail.com>
2020-10-21 10:16:33 +0530

    restore cv option

--- resume/home/views.py
+++ resume/home/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render,HttpResponse,redirect
-from .models import Visitor,Project
+from .models import Visitor,Project,MyCv
 # Create your views here.
+def get_cv():
+    return MyCv.objects.first()
 
 def dashboard(request):
     clicked_on = 'dashboard'
+    cv = get_cv()
     data = {
-        'clicked_on':clicked_on
+        'clicked_on':clicked_on,
+        'cv':cv
     }
     return render(request,'home/dashboard.html',data)
 
@@ -13,8 +17,11 @@ def dashboard(request):
 def contact(request):
     print("=== post data ",request.POST)
     clicked_on = 'contact'
+    cv = get_cv()
     data = {
-        'clicked_on':clicked_on    }
+        'clicked_on':clicked_on,
+        'cv':cv
+    }
     if request.method == "POST":
         name = request.POST.get('uname')
         email = request.POST.get('email')
@@ -33,14 +40,16 @@ def contact(request):
         )
         visit.save()
 
-        return render(request,'home/success_message.html',{'name':name,'clicked_on':clicked_on})
+        return render(request,'home/success_message.html',{'name':name,'clicked_on':clicked_on,'cv':cv})
     return render(request,'home/contact.html',data)
 
 
 def about(request):
     clicked_on = 'about'
+    cv = get_cv()
     data = {
-        'clicked_on':clicked_on
+        'clicked_on':clicked_on,
+        'cv':cv
     }
     return render(request,'home/about.html',data)
     
@@ -49,9 +58,10 @@ def about(request):
 def project(request):
     project_list = Project.objects.all()
     clicked_on = 'project'
-
+    cv = get_cv()
     data = {
         'projects':project_list,
-        'clicked_on':clicked_on
+        'clicked_on':clicked_on,
+        'cv':cv
     }
     return render(request,'home/project.html',data)
